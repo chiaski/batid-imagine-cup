180ad0253564eaa56bd74c13670befc74cc15cab
SirBob <kleonardo52@gmail.com>
2018-02-23 18:46:07 +0800

    fixed mysqli_error($conn);

--- index.php
+++ index.php
@@ -3,22 +3,18 @@
 	$host = "batidlive.mysql.database.azure.com";
 	$user = "keithyadmin@batidlive";
 	$pass = "Franzlispogi1!";
-
 	$conn = mysqli_init();
 	mysqli_real_connect($conn, $host, $user, $pass, $db);
-
 	if (mysqli_connect_errno($conn)) {
 		die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
 	else {
 		echo("Connected successfully!");
 	}
-
 	$query = mysqli_query($conn, 'SELECT * FROM reports');
 	if (!$query) {
-		die('Invalid query: ' . mysql_error());
+		die('Invalid query: ' . mysqli_error($conn));
 	}
-
 	$result = mysqli_fetch_assoc($query);
 	?>
 	<html>
