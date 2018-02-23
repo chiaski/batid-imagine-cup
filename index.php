7fd7247da3a4b20af99b55bd08a478431e594791
SirBob <kleonardo52@gmail.com>
2018-02-23 14:37:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	$db = "batid_db";
+	$host = "batidlive.mysql.database.azure.com";
+	$user = "keithyadmin@batidlive";
+	$pass = "Franzlispogi1!";
+
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $user, $pass, $db);
+
+	if (mysqli_connect_errno($conn)) {
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+	else {
+		echo("Connected successfully!");
+	}
+?>
