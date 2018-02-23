72ba8831a8fc2b75892a5959891c2d10c1cfe728
leroy-bulaong <30501618+leroy-bulaong@users.noreply.github.com>
2018-02-23 19:09:29 +0800

    thingoreing

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 	$host = "batidlive.mysql.database.azure.com";
 	$user = "keithyadmin@batidlive";
 	$pass = "Franzlispogi1!";
-	$conn = mysqli_init();
-	mysqli_real_connect($conn, $host, $user, $pass, $db);
+	$conn = mysqli_connect($host, $user, $pass, $db);
 	if (mysqli_connect_errno($conn)) {
 		die('Failed to connect to MySQL: '.mysqli_connect_error());
 	}
