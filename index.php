6969dedcd593cc891c65a93a884610035cb4efd4
leroy-bulaong <30501618+leroy-bulaong@users.noreply.github.com>
2018-02-23 18:07:17 +0800

    Display SQL Data as HTML Table

--- index.php
+++ index.php
@@ -20,8 +20,29 @@
 	}
 
 	$result = mysqli_fetch_assoc($query);
+	?>
+	<html>
+		<head>
+			<title>
+				Batid
+			</title>
+		</head>
+		<body>
+			<table>
 
-	foreach ($data as $result) {
-		echo('<h3>' . $data . '</h3></br>');
+<?php
+	while ($result = mysqli_fetch_assoc($query)) {
+		echo "<tr>";
+			echo "<td>" . $row['longitude'] . "</td>";
+			echo "<td>" . $row['latitude'] . "</td>";
+			echo "<td>" . $row['radius'] . "</td>";
+			echo "<td>" . $row['author'] . "</td>";
+			echo "<td>" . $row['title'] . "</td>";
+			echo "<td>" . $row['content'] . "</td>";
+			echo "<td>" . $row['severity'] . "</td>";
+			echo "<td>" . $row['upvotes'] . "</td>";
+			echo "<td>" . $row['downvotes'] . "</td>";
+		echo "</tr>";
 	}
 ?>
+		</body>
