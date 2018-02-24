ad61510d94a6e1c3a80e9654d1048e866ee7ce5e
SirBob <kleonardo52@gmail.com>
2018-02-24 21:09:22 +0800

    fix version

--- index.php
+++ index.php
@@ -3,52 +3,34 @@
 	$host = "batidlive.mysql.database.azure.com";
 	$user = "keithyadmin@batidlive";
 	$pass = "Franzlispogi1!";
-	$conn = mysqli_connect($host, $user, $pass, $db);
+
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $user, $pass);
+	
 	if (mysqli_connect_errno($conn)) {
-		die('Failed to connect to MySQL: '.mysqli_connect_error());
+		die('Failed to connect to MySQL: ' . mysqli_connect_error());
 	}
 	else {
-		echo("Connected successfully!");
+		echo("Connected successfully!\n");
 	}
 
-	$query = mysqli_query($conn, 'SELECT * FROM batid_db.reports');
+	$query = mysqli_query($conn, "SELECT * FROM batid_db.reports");
+
 	if (!$query) {
-		die('Invalid query: ' . mysqli_error());
+		die('Invalid query: ' . mysqli_error($conn));
 	}
 
-	?>
-	<html>
-		<head>
-			<title>
-				Batid
-			</title>
-		</head>
-		<body>
-			<table>
-				<tr>
-					<td>Longitude</td>
-					<td>Latitude</td>
-					<td>Radius</td>
-					<td>Autho</td>
-					<td>Title</td>
-					<td>Content</td>
-					<td>Severity</td>
-					<td>Upvotes</td>
-					<td>Downvotes</td>
-				</tr>
-<?php
 	while ($result = mysqli_fetch_assoc($query)) {
 		echo "<tr>";
-			echo "<td>" . $row['longitude'] . "</td>";
-			echo "<td>" . $row['latitude'] . "</td>";
-			echo "<td>" . $row['radius'] . "</td>";
-			echo "<td>" . $row['author'] . "</td>";
-			echo "<td>" . $row['title'] . "</td>";
-			echo "<td>" . $row['content'] . "</td>";
-			echo "<td>" . $row['severity'] . "</td>";
-			echo "<td>" . $row['upvotes'] . "</td>";
-			echo "<td>" . $row['downvotes'] . "</td>";
+			echo "<td>" . $result['longitude'] . "</td>";
+			echo "<td>" . $result['latitude'] . "</td>";
+			echo "<td>" . $result['radius'] . "</td>";
+			echo "<td>" . $result['author'] . "</td>";
+			echo "<td>" . $result['title'] . "</td>";
+			echo "<td>" . $result['content'] . "</td>";
+			echo "<td>" . $result['severity'] . "</td>";
+			echo "<td>" . $result['upvotes'] . "</td>";
+			echo "<td>" . $result['downvotes'] . "</td>";
 		echo "</tr>";
 	}
 ?>
-		</body>
