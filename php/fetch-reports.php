b7c2175c67f52eb04231cbf88f7345103177bee5
SirBob <kleonardo52@gmail.com>
2018-03-01 18:12:49 +0800

    move mysql connection to config.php

--- php/fetch-reports.php
+++ php/fetch-reports.php
@@ -1,21 +1,5 @@
 <?php
-	$db = "batid_db";
-	$host = "batidlive.mysql.database.azure.com";
-	$user = "keithyadmin@batidlive";
-	$pass = "Franzlispogi1!";
-
-	$conn = mysqli_init();
-	mysqli_real_connect($conn, $host, $user, $pass);
-	
-	if (mysqli_connect_errno($conn)) {
-		die('Failed to connect to MySQL: ' . mysqli_connect_error());
-	}
-
-	$query = mysqli_query($conn, "SELECT * FROM batid_db.reports");
-
-	if (!$query) {
-		die('Invalid query: ' . mysqli_error($conn));
-	}
+	require_once "config.php";
 
 	$all_reports = array();
 	while ($report = mysqli_fetch_assoc($query)) {
