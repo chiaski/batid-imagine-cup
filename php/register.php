50854d48591f9110fb206b5651fd3b7d8c2c2265
SirBob <kleonardo52@gmail.com>
2018-03-01 18:54:13 +0800

    fix database table name

--- php/register.php
+++ php/register.php
@@ -48,7 +48,7 @@
 		}
 		
 		if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-			$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+			$sql = "INSERT INTO batid_db.users (username, password) VALUES (?, ?)";
 			 
 			if($stmt = mysqli_prepare($conn, $sql)){
 				mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
